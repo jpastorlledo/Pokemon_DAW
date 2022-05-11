389553eb0fc5f5b139eb2aa1a23079b58351a62b
pacomteos <pacomateos03@gmail.com>
2022-05-11 13:26:16 +0200

    Conexion con base de datos.

--- src/pokemon/App.java
+++ src/pokemon/App.java
@@ -1,6 +1,35 @@
 package pokemon;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Connection conec;
+        String url="jdbc:mysql://localhost:3306/pokemon";
+        String login="root";
+        String pass="";
+ 
+        try{
+ 
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conec = DriverManager.getConnection(url, login, pass);
+            System.out.println(conec.getCatalog());
+ 
+            System.out.println("Conexión establecida");
+ 
+            try{
+                conec.close();
+                System.out.println("Conexión cerrada");
+            }catch(SQLException e3){
+                e3.printStackTrace();
+            }
+ 
+        }catch(ClassNotFoundException e1){
+            e1.printStackTrace();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
