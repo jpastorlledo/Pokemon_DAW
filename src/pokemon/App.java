5e95420b4995965399a581f013ed9f40b8b0ae16
pacomteos <pacomateos03@gmail.com>
2022-05-12 10:14:07 +0200

    Implementación de los javafx en referenced libraries.

--- src/pokemon/App.java
+++ src/pokemon/App.java
@@ -1,6 +1,16 @@
 package pokemon;
-public class App {
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application{
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        
     }
 }
