6558be66a1582e7f54a8738914c1dad5ac03f5b1
pacomteos <pacomateos03@gmail.com>
2022-05-12 10:31:01 +0200

    Commit introduciendo JavaFX.

--- src/pokemon/App.java
+++ src/pokemon/App.java
@@ -1,9 +1,16 @@
 package pokemon;
 
 import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class App {
+public class App extends Application {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        
     }
 }
