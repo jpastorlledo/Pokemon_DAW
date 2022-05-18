ba3f05faead25c947e3f8a261a07ac49a7207cbc
jpastorlledo <josemanuel.s280147@cesurformacion.com>
2022-05-18 13:20:18 +0200

    Cambio en la clase test.

--- src/pokemon/Test.java
+++ src/pokemon/Test.java
@@ -19,11 +19,11 @@ public class Test {
         Pokemon p1, p2, p3, p4, p5;
         Entrenador e1;
 
-        p1 = new Pokemon(1, "P1", "P1", 1, Tipo.AGUA, Tipo.VOLADOR, Estado.SIN_ESTADO);
-        p2 = new Pokemon(2, "P2", "P2", 2, Tipo.BICHO, Tipo.TIERRA, Estado.SIN_ESTADO);
-        p3 = new Pokemon(3, "P3", "P3", 3, Tipo.ELECTRICO, Tipo.AGUA, Estado.SIN_ESTADO);
-        p4 = new Pokemon(4, "P4", "P4", 4, Tipo.FUEGO, null, Estado.SIN_ESTADO);
-        p5 = new Pokemon(5, "P5", "P5", 5, Tipo.PLANTA, null, Estado.SIN_ESTADO);
+        p1 = new Pokemon(1, "P1", "P1", 1, Tipo.AGUA, Tipo.VOLADOR, Estado.SIN_ESTADO, 20);
+        p2 = new Pokemon(2, "P2", "P2", 2, Tipo.BICHO, Tipo.TIERRA, Estado.SIN_ESTADO, 30);
+        p3 = new Pokemon(3, "P3", "P3", 3, Tipo.ELECTRICO, Tipo.AGUA, Estado.SIN_ESTADO, 40);
+        p4 = new Pokemon(4, "P4", "P4", 4, Tipo.FUEGO, null, Estado.SIN_ESTADO, 30);
+        p5 = new Pokemon(5, "P5", "P5", 5, Tipo.PLANTA, null, Estado.SIN_ESTADO, 50);
 
         Pokemon []equipo = {p1, p3, p4};
         Pokemon []secundario = new Pokemon[100];
