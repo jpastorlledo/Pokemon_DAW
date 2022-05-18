c622b55fff5beb59c1fb59125d144585e6afe864
pacomteos <pacomateos03@gmail.com>
2022-05-18 21:25:50 +0200

    Creación de la clase ElegirController y modificacion de TestController

--- src/controladores/TestController.java
+++ src/controladores/TestController.java
@@ -1,21 +1,34 @@
 package controladores;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class TestController implements Initializable{
 
     @FXML
     private Button btnEntrar;
+    private Object stage;
 
     @FXML
-    private void entrar(ActionEvent event){
-        System.out.println("Hello World!");
+    private void entrar(ActionEvent event)throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/btnEntrar.FXML"));
+        Parent root = loader.load();
+        ElegirController controller = loader.getController();
+        Scene scene = new Scene(root);
+        Stage stage = new Stage();
+        stage.showAndWait();
+        ((Stage) this.stage).close();
+
     }
 
 
