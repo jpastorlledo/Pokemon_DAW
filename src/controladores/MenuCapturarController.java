82636f59b9ee42d6e66a3d1b61320b4969fd995f
pacomteos <pacomateos03@gmail.com>
2022-05-20 10:54:29 +0200

    error al declarar boton

--- src/controladores/MenuCapturarController.java
+++ src/controladores/MenuCapturarController.java
@@ -24,7 +24,7 @@ public class MenuCapturarController implements Initializable{
     private Button btnGenPok;
 
     @FXML 
-    Button btnCapturarPokemon;
+    private Button btnCapturarPokemon;
    
 
     @FXML
